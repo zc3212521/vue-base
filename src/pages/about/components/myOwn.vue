500085c6fcec2d7b5fe5f092e132b89ee3b87f05
zhangchao <13075311322@189.cn>
2019-02-15 18:05:14 +0800

    init

---
+++ src/pages/about/components/myOwn.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>This is my component</h3>
+</template>
+
+<script>
+export default {
+  name: 'myOwn'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/business/HelloWorld.vue'
 import { getTestData } from '@/services/moduleA.js'
 
 export default {
